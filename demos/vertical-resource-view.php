aef013117a4b5eb10cb4c38b73b1ad558553c23d
Edo Freriks <edofre@gmail.com>
2016-12-06 22:45:52 +0100

    Added vertical-resource-view demo

---
+++ demos/vertical-resource-view.php
@@ -0,0 +1,68 @@
+<?php
+// Generate a new fullcalendar instance
+$calendar = new \Edofre\FullcalendarScheduler\FullcalendarScheduler($view);
+
+// Set the events and resources
+$calendar->setEvents([
+    ['id' => '1', 'resourceId' => 'a', 'start' => '2016-05-06', 'end' => '2016-05-08', 'title' => 'event 1'],
+    ['id' => '2', 'resourceId' => 'a', 'start' => '2016-05-07T09:00:00', 'end' => '2016-05-07T14:00:00', 'title' => 'event 2'],
+    ['id' => '3', 'resourceId' => 'b', 'start' => '2016-05-07T12:00:00', 'end' => '2016-05-08T06:00:00', 'title' => 'event 3'],
+    ['id' => '4', 'resourceId' => 'c', 'start' => '2016-05-07T07:30:00', 'end' => '2016-05-07T09:30:00', 'title' => 'event 4'],
+    ['id' => '5', 'resourceId' => 'd', 'start' => '2016-05-07T10:00:00', 'end' => '2016-05-07T15:00:00', 'title' => 'event 5'],
+]);
+// Add the resources
+$calendar->setResources([
+    ['id' => 'a', 'title' => 'Room A'],
+    ['id' => 'b', 'title' => 'Room B', 'eventColor' => 'green'],
+    ['id' => 'c', 'title' => 'Room C', 'eventColor' => 'orange'],
+    ['id' => 'd', 'title' => 'Room D', 'eventColor' => 'red'],
+]);
+
+$calendar->setCallbacks([
+    'select'   => "
+			function(start, end, jsEvent, view, resource) {
+				console.log(
+					'select',
+					start.format(),
+					end.format(),
+					resource ? resource.id : '(no resource)'
+				);
+			}
+		",
+    'dayClick' => "
+			function(date, jsEvent, view, resource) {
+				console.log(
+					'dayClick',
+					date.format(),
+					resource ? resource.id : '(no resource)'
+				);
+			}
+		",
+]);
+
+// Set options
+$calendar->setOptions([
+    'header'      => [
+        'left'   => 'prev,next today',
+        'center' => 'title',
+        'right'  => 'agendaDay,agendaTwoDay,agendaWeek,month',
+    ],
+    'defaultView' => 'agendaDay',
+    'defaultDate' => '2016-05-07',
+    'editable'    => true,
+    'selectable'  => true,
+    'eventLimit'  => true, // allow "more" link when too many events
+    'views'       => [
+        'agendaTwoDay' => [
+            'type'            => 'agenda',
+            'duration'        => ['days' => 2],
+            // views that are more than a day will NOT do this behavior by default
+            // so, we need to explicitly enable it
+            'groupByResource' => true,
+            // uncomment this line to group by day FIRST with resources underneath
+            //'groupByDateAndResource'=>true,
+        ],
+    ],
+    // uncomment this line to hide the all-day slot
+    //'allDaySlot'=>false,
+]);
