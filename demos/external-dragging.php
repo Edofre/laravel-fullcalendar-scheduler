4babeed366ca9813f07ad54fe9ef6809dc36347a
Edo Freriks <edofre@gmail.com>
2016-12-10 16:07:36 +0100

    Added external-dragging demo

--- demos/external-dragging.php
+++ demos/external-dragging.php
@@ -1,169 +1,4 @@
 <?php
-
-/**
- * IMPORTANT
- *
- * For the draggables to work make sure you include the 'yii\jui\JuiAsset' asset to your AppAsset depends configuration
- *
- * IMPORTANT
- */
-
-$this->registerJs("
-	/* initialize the external events
-	-----------------------------------------------------------------*/
-	$('#external-events .fc-event').each(function() {
-
-		// store data so the calendar knows to render an event upon drop
-		$(this).data('event', {
-			title: $.trim($(this).text()), // use the element's text as the event title
-			stick: true // maintain when user navigates (see docs on the renderEvent method)
-		});
-
-		// make the event draggable using jQuery UI
-		$(this).draggable({
-			zIndex: 999,
-			revert: true,      // will cause the event to go back to its
-			revertDuration: 0  //  original position after the drag
-		});
-	});
-", \yii\web\View::POS_END);
-?>
-
-<?= \edofre\fullcalendarscheduler\FullcalendarScheduler::widget([
-	'clientOptions' => [
-
-		'resources'         => [
-			['id' => 'a', 'title' => 'Auditorium A'],
-			['id' => 'b', 'title' => 'Auditorium B', 'eventColor' => 'green'],
-			['id' => 'c', 'title' => 'Auditorium C', 'eventColor' => 'orange'],
-			[
-				'id'       => 'd', 'title' => 'Auditorium D',
-				'children' => [
-					['id' => 'd1', 'title' => 'Room D1'],
-					['id' => 'd2', 'title' => 'Room D2'],
-				],
-			],
-			['id' => 'e', 'title' => 'Auditorium E'],
-			['id' => 'f', 'title' => 'Auditorium F', 'eventColor' => 'red'],
-			['id' => 'g', 'title' => 'Auditorium G'],
-			['id' => 'h', 'title' => 'Auditorium H'],
-			['id' => 'i', 'title' => 'Auditorium I'],
-			['id' => 'j', 'title' => 'Auditorium J'],
-			['id' => 'k', 'title' => 'Auditorium K'],
-			['id' => 'l', 'title' => 'Auditorium L'],
-			['id' => 'm', 'title' => 'Auditorium M'],
-			['id' => 'n', 'title' => 'Auditorium N'],
-			['id' => 'o', 'title' => 'Auditorium O'],
-			['id' => 'p', 'title' => 'Auditorium P'],
-			['id' => 'q', 'title' => 'Auditorium Q'],
-			['id' => 'r', 'title' => 'Auditorium R'],
-			['id' => 's', 'title' => 'Auditorium S'],
-			['id' => 't', 'title' => 'Auditorium T'],
-			['id' => 'u', 'title' => 'Auditorium U'],
-			['id' => 'v', 'title' => 'Auditorium V'],
-			['id' => 'w', 'title' => 'Auditorium W'],
-			['id' => 'x', 'title' => 'Auditorium X'],
-			['id' => 'y', 'title' => 'Auditorium Y'],
-			['id' => 'z', 'title' => 'Auditorium Z'],
-		],
-		'events'            => [
-
-		],
-		'drop'         => new \yii\web\JsExpression("
-			function(date, jsEvent, ui, resourceId) {
-				console.log('drop', date.format(), resourceId);
-
-				// is the \"remove after drop\" checkbox checked?
-				if ($('#drop-remove').is(':checked')) {
-					// if so, remove the element from the \"Draggable Events\" list
-					$(this).remove();
-				}
-			}
-		"),
-		'eventReceive' => new \yii\web\JsExpression("
-			function(event) { // called when a proper external event is dropped
-				console.log('eventReceive', event);
-			}
-		"),
-		'eventDrop'    => new \yii\web\JsExpression("
-			function(event) { // called when an event (already on the calendar) is moved
-				console.log('eventDrop', event);
-			}
-		"),
-	],
-]);
-?>
-<style>
-	body {
-		margin-top: 40px;
-		text-align: center;
-		font-size: 14px;
-		font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-	}
-
-	#wrap {
-		width: 1100px;
-		margin: 0 auto;
-	}
-
-	#external-events {
-		float: left;
-		width: 150px;
-		padding: 0 10px;
-		border: 1px solid #ccc;
-		background: #eee;
-		text-align: left;
-	}
-
-	#external-events h4 {
-		font-size: 16px;
-		margin-top: 0;
-		padding-top: 1em;
-	}
-
-	#external-events .fc-event {
-		margin: 10px 0;
-		cursor: pointer;
-	}
-
-	#external-events p {
-		margin: 1.5em 0;
-		font-size: 11px;
-		color: #666;
-	}
-
-	#external-events p input {
-		margin: 0;
-		vertical-align: middle;
-	}
-
-	#calendar {
-		float: right;
-		width: 900px;
-	}
-</style>
-
-<div id='wrap'>
-	<div id='external-events'>
-		<h4>Draggable Events</h4>
-		<div class='fc-event'>My Event 1</div>
-		<div class='fc-event'>My Event 2</div>
-		<div class='fc-event'>My Event 3</div>
-		<div class='fc-event'>My Event 4</div>
-		<div class='fc-event'>My Event 5</div>
-		<p>
-			<input type='checkbox' id='drop-remove'/>
-			<label for='drop-remove'>remove after drop</label>
-		</p>
-	</div>
-
-	<div id='calendar'></div>
-
-	<div style='clear:both'></div>
-
-</div>
-
-<?php
 // Generate a new fullcalendar instance
 $calendar = new \Edofre\FullcalendarScheduler\FullcalendarScheduler($view);
 
@@ -177,7 +12,61 @@ $calendar->setEvents([
 ]);
 // Add the resources
 $calendar->setResources([
+    ['id' => 'a', 'title' => 'Auditorium A'],
+    ['id' => 'b', 'title' => 'Auditorium B', 'eventColor' => 'green'],
+    ['id' => 'c', 'title' => 'Auditorium C', 'eventColor' => 'orange'],
+    [
+        'id'       => 'd', 'title' => 'Auditorium D',
+        'children' => [
+            ['id' => 'd1', 'title' => 'Room D1'],
+            ['id' => 'd2', 'title' => 'Room D2'],
+        ],
+    ],
+    ['id' => 'e', 'title' => 'Auditorium E'],
+    ['id' => 'f', 'title' => 'Auditorium F', 'eventColor' => 'red'],
+    ['id' => 'g', 'title' => 'Auditorium G'],
+    ['id' => 'h', 'title' => 'Auditorium H'],
+    ['id' => 'i', 'title' => 'Auditorium I'],
+    ['id' => 'j', 'title' => 'Auditorium J'],
+    ['id' => 'k', 'title' => 'Auditorium K'],
+    ['id' => 'l', 'title' => 'Auditorium L'],
+    ['id' => 'm', 'title' => 'Auditorium M'],
+    ['id' => 'n', 'title' => 'Auditorium N'],
+    ['id' => 'o', 'title' => 'Auditorium O'],
+    ['id' => 'p', 'title' => 'Auditorium P'],
+    ['id' => 'q', 'title' => 'Auditorium Q'],
+    ['id' => 'r', 'title' => 'Auditorium R'],
+    ['id' => 's', 'title' => 'Auditorium S'],
+    ['id' => 't', 'title' => 'Auditorium T'],
+    ['id' => 'u', 'title' => 'Auditorium U'],
+    ['id' => 'v', 'title' => 'Auditorium V'],
+    ['id' => 'w', 'title' => 'Auditorium W'],
+    ['id' => 'x', 'title' => 'Auditorium X'],
+    ['id' => 'y', 'title' => 'Auditorium Y'],
+    ['id' => 'z', 'title' => 'Auditorium Z'],
+]);
 
+$calendar->setCallbacks([
+    'drop'         => "
+            function(date, jsEvent, ui, resourceId) {
+                console.log('drop', date.format(), resourceId);
+                // is the \"remove after drop\" checkbox checked?
+                if ($('#drop-remove').is(':checked')) {
+                    // if so, remove the element from the \"Draggable Events\" list
+                    $(this).remove();
+                }
+            }
+        ",
+    'eventReceive' => "
+            function(event) { // called when a proper external event is dropped
+                console.log('eventReceive', event);
+            }
+        ",
+    'eventDrop'    => "
+            function(event) { // called when an event (already on the calendar) is moved
+                console.log('eventDrop', event);
+            }
+        ",
 ]);
 
 // Set options
@@ -201,3 +90,85 @@ $calendar->setOptions([
     ],
     'resourceLabelText' => 'Rooms',
 ]);
+?>
+
+<script>
+    $(document).ready(function () {
+        /* initialize the external events
+         -----------------------------------------------------------------*/
+        $('#external-events .fc-event').each(function () {
+
+            // store data so the calendar knows to render an event upon drop
+            $(this).data('event', {
+                title: $.trim($(this).text()), // use the element's text as the event title
+                stick: true // maintain when user navigates (see docs on the renderEvent method)
+            });
+
+            // make the event draggable using jQuery UI
+            $(this).draggable({
+                zIndex: 999,
+                revert: true,      // will cause the event to go back to its
+                revertDuration: 0  //  original position after the drag
+            });
+        });
+    });
+</script>
+
+<style>
+    body {
+        margin-top: 40px;
+        text-align: center;
+        font-size: 14px;
+        font-family: "Lucida Grande", Helvetica, Arial, Verdana, sans-serif;
+    }
+
+    #external-events {
+        float: left;
+        width: 150px;
+        padding: 0 10px;
+        border: 1px solid #ccc;
+        background: #eee;
+        text-align: left;
+    }
+
+    #external-events h4 {
+        font-size: 16px;
+        margin-top: 0;
+        padding-top: 1em;
+    }
+
+    #external-events .fc-event {
+        margin: 10px 0;
+        cursor: pointer;
+    }
+
+    #external-events p {
+        margin: 1.5em 0;
+        font-size: 11px;
+        color: #666;
+    }
+
+    #external-events p input {
+        margin: 0;
+        vertical-align: middle;
+    }
+</style>
+
+<div class="col-md-2">
+    <div id='external-events'>
+        <h4>Draggable Events</h4>
+        <div class='fc-event'>My Event 1</div>
+        <div class='fc-event'>My Event 2</div>
+        <div class='fc-event'>My Event 3</div>
+        <div class='fc-event'>My Event 4</div>
+        <div class='fc-event'>My Event 5</div>
+        <p>`
+            <input type='checkbox' id='drop-remove'/>
+            <label for='drop-remove'>remove after drop</label>
+        </p>
+    </div>
+</div>
+
+<div class="col-md-10">
+    {!! $calendar->generate() !!}
+</div>
